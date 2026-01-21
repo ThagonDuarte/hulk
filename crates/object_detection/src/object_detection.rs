20276aa57bd4cdc9385722ef9dee9800dcde2fb0
Alexander Schmidt <alexander.schmidt@tuhh.de>
2026-01-21 21:03:46 +0100

    Nao label party yolo11m finetune
    Co-authored-by: Franziska-Sophie <74772344+Franziska-Sophie@users.noreply.github.com>

--- crates/object_detection/src/object_detection.rs
+++ crates/object_detection/src/object_detection.rs
@@ -19,7 +19,7 @@ use ros2::sensor_msgs::{camera_info::CameraInfo, image::Image};
 use serde::{Deserialize, Serialize};
 use types::{
     bounding_box::BoundingBox,
-    object_detection::{Detection, YOLOv8ObjectDetectionLabel},
+    object_detection::{Detection, NaoLabelPartyObjectDetectionLabel, YOLOv8ObjectDetectionLabel},
     parameters::ObjectDetectionParameters,
 };
 
@@ -58,7 +58,9 @@ impl ObjectDetection {
                 TensorRTExecutionProvider::default().build(),
                 CUDAExecutionProvider::default().build(),
             ])?
-            .commit_from_file(neural_network_folder.join("yolo11n.onnx"))?;
+            .commit_from_file(
+                neural_network_folder.join("teamfahrt-label-party-yolo11m-640x480.onnx"),
+            )?;
 
         Ok(Self { session })
     }
@@ -108,7 +110,7 @@ impl ObjectDetection {
                 if *confidence < context.parameters.confidence_threshold {
                     return None;
                 }
-                let label = YOLOv8ObjectDetectionLabel::from_index(class_id);
+                let label = NaoLabelPartyObjectDetectionLabel::from_index(class_id);
                 Some(Detection {
                     bounding_box: BoundingBox {
                         area: Rectangle::new_with_center_and_size(
