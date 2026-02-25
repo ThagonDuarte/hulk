a2a3c22bad7eda2d1f7dc78e28618f976bd4cca6
Alexander Schmidt <alexander.schmidt@tuhh.de>
2026-02-25 10:28:01 +0100

    Walking policy inference of booster_deploy repo

--- crates/walking_inference/src/inference.rs
+++ crates/walking_inference/src/inference.rs
@@ -1,4 +1,4 @@
-use std::{path::Path, time::Duration};
+use std::{collections::VecDeque, path::Path, time::Duration};
 
 use booster::{ImuState, MotorCommandParameters, MotorState};
 use color_eyre::Result;
@@ -25,7 +25,8 @@ pub struct WalkingInference {
     last_linear_velocity_command: Vector2<Ground>,
     last_angular_velocity_command: f32,
     last_gait_progress: f32,
-    pub last_target_joint_positions: Joints,
+    last_target_joint_positions: Joints,
+    input_history: VecDeque<WalkingInferenceInputs>,
 }
 
 impl WalkingInference {
@@ -45,12 +46,18 @@ impl WalkingInference {
             ])?
             .commit_from_file(neural_network_path)?;
 
+        let mut input_history = VecDeque::with_capacity(10);
+        for _ in 0..10 {
+            input_history.push_front(Default::default());
+        }
+
         Ok(Self {
             session,
             last_linear_velocity_command: vector![0.0, 0.0],
             last_angular_velocity_command: 0.0,
             last_gait_progress: 0.0,
             last_target_joint_positions: prepare_motor_command_parameters.default_positions,
+            input_history,
         })
     }
 
@@ -82,11 +89,23 @@ impl WalkingInference {
         self.last_angular_velocity_command = walking_inference_inputs.angular_velocity_command;
         self.last_gait_progress = walking_inference_inputs.gait_progress;
 
-        let inputs: Array1<f32> = walking_inference_inputs
-            .mjlab_walking_policy_obersvation_vector()
+        self.input_history.push_front(walking_inference_inputs);
+        self.input_history
+            .truncate(walking_parameters.observation_history_length);
+
+        let inputs: Array1<f32> = self
+            .input_history
+            .iter()
+            .rev()
+            .flat_map(|inputs| inputs.mjlab_walking_policy_obersvation_vector())
+            .collect::<Vec<f32>>()
             .into();
 
-        assert!(inputs.len() == walking_parameters.number_of_observations);
+        assert!(
+            inputs.len()
+                == walking_parameters.number_of_observations
+                    * walking_parameters.observation_history_length
+        );
         let inputs_tensor = Tensor::from_array(inputs.insert_axis(Axis(0)))?;
 
         let inference_input = inputs![inputs_tensor];
