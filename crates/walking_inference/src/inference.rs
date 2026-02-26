65c5afb4587aadd576b7207d9c53a2df5afa08ff
Ben Sampaolo <Ben.sampaolo@tuhh.de>
2026-02-26 19:04:01 +0100

    New model 2026-02-26_18-09-07

--- crates/walking_inference/src/inference.rs
+++ crates/walking_inference/src/inference.rs
@@ -35,7 +35,7 @@ impl WalkingInference {
     ) -> Result<Self> {
         let neural_network_path = neural_network_folder
             .as_ref()
-            .join("2026-02-26_10-43-17.onnx");
+            .join("2026-02-26_18-09-07-1000.onnx");
 
         let session = Session::builder()?
             .with_optimization_level(GraphOptimizationLevel::Level3)?
