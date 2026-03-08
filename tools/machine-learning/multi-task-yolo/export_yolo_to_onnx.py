7302775e0aae628335c465fc8aa352f0459e89c8
Alexander Schmidt <alexander.schmidt@tuhh.de>
2026-03-08 16:29:32 +0100

    --wip--

--- tools/machine-learning/multi-task-yolo/export_yolo_to_onnx.py
+++ tools/machine-learning/multi-task-yolo/export_yolo_to_onnx.py
@@ -5,8 +5,18 @@ from ultralytics import YOLO
 
 def convert_model(model_path: Path, height: int, width: int) -> None:
     model = YOLO(model_path)
-    model.export(format="onnx", imgsz=(height, width), simplify=True, nms=True)
+    model.export(
+        format="onnx",
+        imgsz=(height, width),
+        simplify=True,
+        name=model_path.stem + "-" + str(width) + "x" + str(height) + ".onnx",
+    )
 
 
 if __name__ == "__main__":
-    convert_model(Path("./yolo26m-finetune.pt"), 448, 544)
+    height = 1088 / 2
+    width = 1280 / 2
+
+    convert_model(Path("./yolo26m-finetune.pt"), height, width)
+    convert_model(Path("./yolo26s-finetune.pt"), height, width)
+    convert_model(Path("./yolo26n-finetune.pt"), height, width)
